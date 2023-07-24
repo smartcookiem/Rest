446020e063b938711c70b863a0f38ec122c1c7b8
smartcookiem <svyaznoy1835@gmail.com>
2023-07-25 01:11:35 +0300

    body failed

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 
 class MobileBankApiTestV1 {
@@ -20,6 +21,7 @@ class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[2].currency", equalTo("RUB"))
         ;
 
     }
